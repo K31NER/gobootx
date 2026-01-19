6610a5bd8e3ae2c803a8807e6f58cc3c221956be
K31NER <zunigaromerokeiner@gmail.com>
2026-01-19 13:35:47 -0500

    Feat: Se refactorizo y se agrego la seleccion de lenguaje y creacion de dockerfile

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 
 	"github.com/K31NER/gobootx/cmd"
-	_ "github.com/K31NER/gobootx/internal/clean"
-	_ "github.com/K31NER/gobootx/internal/mvc"
+	_ "github.com/K31NER/gobootx/internal/architectures/clean"
+	_ "github.com/K31NER/gobootx/internal/architectures/mvc"
 )
 
 func main() {
